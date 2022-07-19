79e11bdcac754718ab8f6e8d22274d648378f35c
itota37 <40158522+itota37@users.noreply.github.com>
2022-07-19 12:07:03 +0900

    Update serialization.

--- Engine/Core/source/elekicore/serialization.cpp
+++ Engine/Core/source/elekicore/serialization.cpp
@@ -166,7 +166,7 @@ _ELEKICORE_SERIALIZATION_DATANODE_CONSTRUCTER(Binary, BINARY);
 // -----
 
 template<class T, size_t SIZE = sizeof(T)>
-T readBinaryNumber(const Ref<List<u8>> &binary, size_t index)
+T readBinaryNumber(const UR<List<u8>> &binary, size_t &index)
 {
 	T result;
 	auto outBytes = (u8 *) (void *) &result;
@@ -183,7 +183,85 @@ T readBinaryNumber(const Ref<List<u8>> &binary, size_t index)
 		break;
 	}
 
+	index += SIZE;
 	return result;
+}
+
+String readBinaryString(const UR<List<u8>> &binary, size_t &index)
+{
+	String result = (Char*)&binary->at(index);
+	index += result.count();
+	return result;
+}
+
+UR<Serialization::DataNode> makeDataNode(const UR<List<u8>> &binary, size_t &index);
+
+template<class T, class D>
+UR<Serialization::DataNode> makeNumberDataNode(const UR<List<u8>> &binary, size_t &index)
+{
+	UR<D> result;
+	result->value = readBinaryNumber<T>(binary, index);
+	return (UR<Serialization::DataNode>)result;
+}
+
+UR<Serialization::DataNode> makeReferenceDataNode(const UR<List<u8>> &binary, size_t &index)
+{
+	if((EBinarySign)binary->at(index) == EBinarySign::U32)
+	{
+		UR<Serialization::InsideReferenceDataNode> result;
+		result->value = readBinaryNumber<u32>(binary, ++index);
+		return result;
+	}
+	else
+	{
+		UR<Serialization::OutsideReferenceDataNode> result;
+		result->value = readBinaryString(binary, ++index);
+		return result;
+	}
+}
+
+UR<Serialization::DataNode> makeArrayDataNode(const UR<List<u8>> &binary, size_t &index)
+{
+	UR<Serialization::ArrayDataNode> result;
+	while((EBinarySign)binary->at(index) != EBinarySign::END)
+	{
+		result->value.add(makeDataNode(binary, index));
+	}
+	return (UR<Serialization::DataNode>)result;
+}
+
+UR<Serialization::DataNode> makeStructDataNode(const UR<List<u8>> &binary, size_t &index)
+{
+	UR<Serialization::StructDataNode> result;
+	while((EBinarySign)binary->at(index) != EBinarySign::END)
+	{
+		result->value.add(readBinaryString(binary, ++index), makeDataNode(binary, index));
+	}
+	return (UR<Serialization::DataNode>)result;
+}
+
+UR<Serialization::DataNode> makeDataNode(const UR<List<u8>> &binary, size_t &index)
+{
+	switch((EBinarySign)binary->at(index))
+	{
+		case EBinarySign::I8: return makeNumberDataNode<i8, Serialization::I8DataNode>(binary, ++index);
+		case EBinarySign::U8: return makeNumberDataNode<u8, Serialization::U8DataNode>(binary, ++index);
+		case EBinarySign::I16: return makeNumberDataNode<i16, Serialization::I16DataNode>(binary, ++index);
+		case EBinarySign::U16: return makeNumberDataNode<u16, Serialization::U16DataNode>(binary, ++index);
+		case EBinarySign::I32: return makeNumberDataNode<i32, Serialization::I32DataNode>(binary, ++index);
+		case EBinarySign::U32: return makeNumberDataNode<u32, Serialization::U32DataNode>(binary, ++index);
+		case EBinarySign::I64: return makeNumberDataNode<i64, Serialization::I64DataNode>(binary, ++index);
+		case EBinarySign::U64: return makeNumberDataNode<u64, Serialization::U64DataNode>(binary, ++index);
+		case EBinarySign::F32: return makeNumberDataNode<f32, Serialization::F32DataNode>(binary, ++index);
+		case EBinarySign::F64: return makeNumberDataNode<f64, Serialization::F64DataNode>(binary, ++index);
+		case EBinarySign::NIL: return (UR<Serialization::DataNode>) UR<Serialization::NilDataNode>();
+		case EBinarySign::TRUE: return (UR<Serialization::DataNode>) UR<Serialization::TrueDataNode>();
+		case EBinarySign::FALSE: return (UR<Serialization::DataNode>) UR<Serialization::FalseDataNode>();
+		case EBinarySign::REFERENCE: return makeReferenceDataNode(binary, ++index);
+		case EBinarySign::ARRAY: return makeArrayDataNode(binary, ++index);
+		case EBinarySign::STRUCT: return makeStructDataNode(binary, ++index);
+		default: return UR<Serialization::DataNode>();
+	}
 }
 
 bool isElekiBinary(const UR<List<u8>> &binary)
@@ -198,8 +276,38 @@ bool isElekiBinary(const UR<List<u8>> &binary)
 	return Serialization::BinaryInformation::VERSION == readBinaryNumber<u32>(binary, i);
 }
 
+// 並列読み込み開始位置とサイズを取得します
+void readStartPosition(const UR<List<u8>> &binary, List<size_t> &startPositionList)
+{
+	for(size_t i = Serialization::BinaryInformation::SIZE; i < binary->count();i += (size_t) readBinaryNumber<u32>(binary, i))
+	{
+		startPositionList.add(i);
+	}
+}
+
 // バイナリデータからノードに変換します
 List<UR<Serialization::DataNode>> ElekiEngine::Serialization::toDataNode(const UR<List<u8>> &binary)
 {
+	// ElekiBinaryかチェックします
 	if(!isElekiBinary(binary)) return List<UR<Serialization::DataNode>>();
+	
+	// 並列読み込み開始位置とサイズを取得します
+	List<size_t> startPositionList; 
+	readStartPosition(binary, startPositionList);
+	
+	// 並列にノードへ変換します
+	List<UR<Task<UR<Serialization::DataNode>>>> tasks(startPositionList.count());
+	for(size_t i = 0; i < startPositionList.count(); i++)
+	{
+		tasks[i] = parallel([&binary, &startPositionList, i](){ return makeDataNode(binary, startPositionList[i]);});
+	}
+	
+	// すべての変換を待ちます
+	List<UR<Serialization::DataNode>> result(tasks.count());
+	for(size_t i = 0; i < tasks.count(); i++)
+	{
+		result[i] = tasks[i]->marge();
+	}
+	
+	return result;
 }
